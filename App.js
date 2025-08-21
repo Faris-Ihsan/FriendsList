6f779c9b47d9aa2a1c4790bcd5a420727ad90415
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-08-21 15:29:02 +0700

    101. Navigationsstruktur in eigener Datei deklarieren

--- App.js
+++ App.js
@@ -1,27 +1,7 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
-import SettingsScreen from './screens/SettingsScreen';
-
-const Tab = createBottomTabNavigator();
+import Navigation from "./Navigation";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name = "Freunde" component={HomeScreen}/>
-        <Tab.Screen name = "Einstellungen" component={SettingsScreen}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Navigation/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Navigation.jsx
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+        <Tab.Navigator>
+            <Tab.Screen name = "Freunde" component={HomeScreen}/>
+            <Tab.Screen name = "Einstellungen" component={SettingsScreen}/>
+        </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
