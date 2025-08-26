9e9a5be03f78a71fa444115121afaba8b1a95564
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-08-26 15:19:19 +0700

    119. Einstellungen in einer gruppierten Liste mit SectionList aufführen

--- screens/SettingsScreen.jsx
+++ screens/SettingsScreen.jsx
@@ -1,9 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { SectionList, StyleSheet, Text, View } from 'react-native';
 
 export default function SettingsScreen() {
     return(
         <View style={styles.container}>
-        <Text>Einstellungen</Text>
+        <SectionList
+            sections={[
+                // Abschnitt1 (Section1)
+                {title: 'Abscnitt1', data: [{name: 'Alice'},{name: 'Bob'}]},
+                // Abschnitt1 (Section2)
+                {title: 'Abscnitt2', data: [{name: 'Asep'},{name: 'Ujang'}]},
+            ]}
+            renderItem={({item}) => <Text>{item.name}</Text>}
+        />
         </View>
     );
 }
@@ -14,5 +22,6 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        paddingTop: 50,
     },
 });
