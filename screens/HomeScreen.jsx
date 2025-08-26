b250e68116b771a7979c23d34acffaed503753c3
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-08-26 14:56:04 +0700

    116. Schlüssel von Listeneinträgen mit keyExtractor dynamisch erstellen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -4,14 +4,16 @@ export default function HomeScreen({navigation}) {
     return(
         <View style={styles.container}>
             <FlatList data={[
-                {name: 'Alice'}, 
-                {name: 'Bob'}, 
-                {name: 'Asep'}, 
-                {name: 'Udin'},
+                { name: 'Alice' }, 
+                { name: 'Bob' }, 
+                { name: 'Asep' }, 
+                { name: 'Udin' },
             ]} renderItem={({item}) => {
                 // return <Text>{item.name}</Text>
                 return <Button title={`Gehe zu ${item.name}`} onPress={()=> navigation.navigate('Friend', {name: item.name})}/>
-            }}/>
+            }}
+            keyExtractor={(item) => item.name}
+        />
         </View>
     );
 }
