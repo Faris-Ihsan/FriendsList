cb19982c1ba3155691551ca2978da28889532bcc
Faris-Ihsan <farisihsan32@yahoo.co.id>
2025-09-01 14:22:19 +0700

    129. state für die zu ladenden Daten einführen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import FriendListItem from '../components/FriendListItem';
 
+// keine Daten
+// warten --> langsame Internetverbindung
+// Ladeprobleme
+const dummyData = [
+        { first: 'Alice', last: 'Schmichael', email: 'test1@example.com'}, 
+        { first: 'Bob', last: 'Schmichael', email: 'test2@example.com'}, 
+        { first: 'Asep', last: 'Schmichael', email: 'test3@example.com'}, 
+        { first: 'Udin', last: 'Schmichael', email: 'test4@example.com'},
+    ]
+
 export default function HomeScreen({navigation}) {
+    const [data, setData] = useState(dummyData);
+
     return(
         <View style={styles.container}>
-            <FlatList data={[
-                { first: 'Alice', last: 'Schmichael', email: 'test1@example.com'}, 
-                { first: 'Bob', last: 'Schmichael', email: 'test2@example.com'}, 
-                { first: 'Asep', last: 'Schmichael', email: 'test3@example.com'}, 
-                { first: 'Udin', last: 'Schmichael', email: 'test4@example.com'},
-            ]} renderItem={({item}) => (
+            <FlatList data={data} renderItem={({item}) => (
                 <FriendListItem friend={item} onPress={()=> navigation.navigate('Friend', { friend: item })}/>
             )}
             keyExtractor={(item) => item.email}
